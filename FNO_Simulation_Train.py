e85fda193e5b134f04c5aa0f0a6875f9be6a773b
CHENG CHEN <105932025+chengchencon@users.noreply.github.com>
2024-07-21 22:27:36 -0400

    Update FNO_Simulation_Train.py

--- FNO_Simulation_Train.py
+++ FNO_Simulation_Train.py
@@ -10,7 +10,6 @@ import operator
 from functools import reduce
 from functools import partial
 from timeit import default_timer
-from tensorflow.keras.optimizers import Adam
 torch.manual_seed(3407)
 np.random.seed(0)
 torch.set_num_threads(1)
